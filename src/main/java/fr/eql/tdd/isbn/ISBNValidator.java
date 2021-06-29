f0fd4d8f9e642f5c6fa430c8dab0b79127f0c483
Benoit Chauvet <bchauvet@ecoleql.fr>
2021-06-29 14:56:20 +0200

    step 3 - GREEN

--- src/main/java/fr/eql/tdd/isbn/ISBNValidator.java
+++ src/main/java/fr/eql/tdd/isbn/ISBNValidator.java
@@ -4,6 +4,13 @@ public class ISBNValidator {
 
 	public boolean checkISBN(ISBN isbn) {
 		
+		
+		
+		if (isbn.getValue().length() != 10)
+		{
+			throw new NumberFormatException("Wrong ISBN : must be 10 digits");
+		}
+		
 		int checksum = 0;
 		
 		for(int i=0; i < 10; i++) {
@@ -12,7 +19,5 @@ public class ISBNValidator {
 		
 		return (checksum % 11 == 0) ? true : false;
 	}
-
-	
 	
 }
