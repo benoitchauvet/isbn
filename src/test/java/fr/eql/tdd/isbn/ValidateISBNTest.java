803b0e3ba61184ae6cab53d24513905c8dc3699d
Benoit Chauvet <bchauvet@ecoleql.fr>
2021-06-29 14:39:11 +0200

    step 3 - RED

--- src/test/java/fr/eql/tdd/isbn/ValidateISBNTest.java
+++ src/test/java/fr/eql/tdd/isbn/ValidateISBNTest.java
@@ -1,12 +1,14 @@
 package fr.eql.tdd.isbn;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
 public class ValidateISBNTest {
 
+	// STEP 1
 	@Test
 	public void checkValidISBN() {
 		ISBNValidator validator = new ISBNValidator();
@@ -15,12 +17,24 @@ public class ValidateISBNTest {
 		assertTrue(result);
 	}
 
+	// STEP 2
 	@Test
 	public void checkInvalidISBN() {
 		ISBNValidator validator = new ISBNValidator();
 		ISBN isbn = new ISBN("1654178167");
 		boolean result = validator.checkISBN(isbn);
 		assertFalse(result);
+	}
+	
+	// STEP 3
+	@Test
+	public void nineDigitsISBNIsNotAllowed() {
+		ISBNValidator validator = new ISBNValidator();
+		ISBN isbn = new ISBN("123456789");
+		
+		assertThrows(NumberFormatException.class, () -> {
+			validator.checkISBN(isbn);
+		});
 	}
 
 }
