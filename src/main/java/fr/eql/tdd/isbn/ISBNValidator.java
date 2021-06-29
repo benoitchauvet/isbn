afa824c73844411b0d97e37dacfbaede60c6c506
Benoit Chauvet <bchauvet@ecoleql.fr>
2021-06-29 15:05:07 +0200

    step 4 - GREEN

--- src/main/java/fr/eql/tdd/isbn/ISBNValidator.java
+++ src/main/java/fr/eql/tdd/isbn/ISBNValidator.java
@@ -3,12 +3,19 @@ package fr.eql.tdd.isbn;
 public class ISBNValidator {
 
 	public boolean checkISBN(ISBN isbn) {
-		
-		
+
 		
 		if (isbn.getValue().length() != 10)
 		{
 			throw new NumberFormatException("Wrong ISBN : must be 10 digits");
+		}
+		
+		
+		for (int i = 0 ; i < 10; i++) {
+			if (! Character.isDigit(isbn.getValue().charAt(i)))
+			{
+				throw new NumberFormatException("Wrong ISBN : must contain only digits");
+			}
 		}
 		
 		int checksum = 0;
