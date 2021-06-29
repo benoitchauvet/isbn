31617cb8872c6053f8f893499039c998408b9b4f
Benoit Chauvet <bchauvet@ecoleql.fr>
2021-06-29 14:21:08 +0200

    step 2 - RED

--- src/test/java/fr/eql/tdd/isbn/ValidateISBNTest.java
+++ src/test/java/fr/eql/tdd/isbn/ValidateISBNTest.java
@@ -1,19 +1,26 @@
 package fr.eql.tdd.isbn;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
 public class ValidateISBNTest {
 
-	
 	@Test
-	public void checkValidISBN()
-	{
+	public void checkValidISBN() {
 		ISBNValidator validator = new ISBNValidator();
 		ISBN isbn = new ISBN("1654178160");
 		boolean result = validator.checkISBN(isbn);
 		assertTrue(result);
 	}
-	
+
+	@Test
+	public void checkInvalidISBN() {
+		ISBNValidator validator = new ISBNValidator();
+		ISBN isbn = new ISBN("1654178167");
+		boolean result = validator.checkISBN(isbn);
+		assertFalse(result);
+	}
+
 }
